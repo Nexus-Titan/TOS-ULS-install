2daded16697dd4cf172b2a556b36c1a08e37c0c5
Tiwut <145930346+tiwut@users.noreply.github.com>
2026-05-17 12:13:17 +0200

    Update dependencies.sh

--- arm64/arch/dependencies.sh
+++ arm64/arch/dependencies.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+pacman -Sy --noconfirm --needed cryptsetup util-linux gawk mkinitcpio coreutils
+
+echo "Dependencies for TiwutOS-ULS successfully installed."
